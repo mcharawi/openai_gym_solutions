79604975783d57523c066aaca091c1ab35332a14
tgarg93 <tushargarg93@gmail.com>
2016-07-23 13:02:16 -0700

    Updated env

--- test.py
+++ test.py
@@ -1,8 +1,15 @@
 __author__ = 'tushar'
 import gym
-
 env = gym.make('FrozenLake-v0')
-env.reset()
-for _ in range(1000):
-    env.render()
-    env.step(env.action_space.sample()) # take a random action
+for i_episode in range(20):
+    observation = env.reset()
+    for t in range(100):
+        env.render()
+        print "Observation", (observation)
+        print(env.observation_space)
+        print(env.action_space)
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+        if done:
+            print("Episode finished after {} timesteps".format(t+1))
+            break
