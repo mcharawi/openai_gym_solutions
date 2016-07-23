861e5719ac22031b06e85b69c006a1721e02cd9d
tgarg93 <tushargarg93@gmail.com>
2016-07-23 12:38:04 -0700

    Test env
    closes #1

---
+++ test.py
@@ -0,0 +1,8 @@
+__author__ = 'tushar'
+import gym
+
+env = gym.make('FrozenLake-v0')
+env.reset()
+for _ in range(1000):
+    env.render()
+    env.step(env.action_space.sample()) # take a random action
