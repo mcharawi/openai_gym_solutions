72bfafa2336483b760b193369016b39d56ee1181
tgarg93 <tushargarg93@gmail.com>
2016-08-06 01:21:11 -0700

    Basic Q learning framework

--- agent.py
+++ agent.py
@@ -1,20 +1,36 @@
-__author__ = 'tushar'
+import numpy as np
 
 class Agent(object):
 
-    def __init__(self, observation, reward, done, info, action_space):
+    RIGHT = 0
+    LEFT = 1
+    UP = 2
+    DOWN = 3
+
+    def __init__(self, observation, reward, info, action, action_space, done=True):
+        self.q_matrix = np.random.rand(16, 4)
+        self.gamma = 0.5
         self.observation = observation
         self.reward = reward
         self.done = done
         self.info = info
+        self.action = action
         self.action_space = action_space
 
-    def update(self, observation, reward, done, info, action_space):
+    def update(self, observation, reward, info, action, action_space, done=True):
+        old_observation = self.observation
         self.observation = observation
         self.reward = reward
         self.done = done
         self.info = info
+        self.action = action
         self.action_space = action_space
+        self.q_matrix[old_observation, action] = reward + self.gamma * np.amax(self.q_matrix[observation, :])
+        print reward
+        print self.q_matrix
 
-    def action(self):
-        return self.action_space.sample()
+
+    def take_action(self):
+        return np.argmax(self.q_matrix[self.observation, :])
+
+
--- test.py
+++ test.py
@@ -4,22 +4,22 @@ from agent import Agent
 
 env = gym.make('FrozenLake-v0')
 
-
 observation = env.reset()
 action_space = env.action_space
 observation, reward, done, info = env.step(action_space.sample())
-agent = Agent(observation, reward, done, info, action_space)
+agent = Agent(observation, reward, info, action_space.sample(), action_space)
 
-for i_episode in range(20):
+for i_episode in range(100):
     observation = env.reset()
     done = False
     t = 0
     while not done:
         env.render()
-        action = agent.action()
+        action = agent.take_action()
         (observation, reward, done, info) = env.step(action)
-        agent.update(observation, reward, done, info, action_space)
+        agent.update(observation, reward, info, action, action_space)
         t = t + 1
         if done:
-            print("Episode finished after {} timesteps".format(t+1))
             break
+            print("Episode finished after {} timesteps".format(t+1))
+
