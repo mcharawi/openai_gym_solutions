b58cc8ddbcb83f16a25d5c5c0d6475b0ff74c4ab
tgarg93 <tushargarg93@gmail.com>
2016-08-20 00:37:15 -0700

    Bellman equation

--- agent.py
+++ agent.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 class Agent(object):
 
@@ -9,6 +10,7 @@ class Agent(object):
 
     def __init__(self, observation, reward, info, action, action_space, done=True):
         self.q_matrix = np.random.rand(16, 4)
+        self.alpha = 0.5
         self.gamma = 0.5
         self.observation = observation
         self.reward = reward
@@ -16,21 +18,35 @@ class Agent(object):
         self.info = info
         self.action = action
         self.action_space = action_space
+        self.epsilon = 1
+        self.decay = 0.98
 
     def update(self, observation, reward, info, action, action_space, done=True):
         old_observation = self.observation
+
         self.observation = observation
-        self.reward = reward
+        R = 0
+        if reward == 0 and done:
+            R = -100
+        elif reward != 0 and done:
+            R = 100
+        self.reward = R
         self.done = done
         self.info = info
         self.action = action
         self.action_space = action_space
-        self.q_matrix[old_observation, action] = reward + self.gamma * np.amax(self.q_matrix[observation, :])
-        print reward
-        print self.q_matrix
+        self.q_matrix[old_observation, action] = self.q_matrix[old_observation, action] + \
+                                                self.alpha * (self.reward + self.gamma * np.amax(self.q_matrix[observation, :]) -
+                                                self.q_matrix[old_observation, action])
 
+        #print self.q_matrix
+        self.epsilon *= self.decay
 
     def take_action(self):
-        return np.argmax(self.q_matrix[self.observation, :])
+        if random.random() > self.epsilon:
+            return np.argmax(self.q_matrix[self.observation, :])
+        else:
+            return self.action_space.sample()
+        # return np.argmax(self.q_matrix[self.observation, :])
 
 
--- test.py
+++ test.py
@@ -1,15 +1,21 @@
 __author__ = 'tushar'
 import gym
 from agent import Agent
+import json
+
+with open('API.json') as api:
+    data = json.load(api)
 
 env = gym.make('FrozenLake-v0')
+env.monitor.start('/tmp/frozenlake-experiment-9')
 
 observation = env.reset()
 action_space = env.action_space
 observation, reward, done, info = env.step(action_space.sample())
 agent = Agent(observation, reward, info, action_space.sample(), action_space)
+num_episodes = 5000
 
-for i_episode in range(100):
+for i_episode in range(num_episodes):
     observation = env.reset()
     done = False
     t = 0
@@ -23,3 +29,6 @@ for i_episode in range(100):
             break
             print("Episode finished after {} timesteps".format(t+1))
 
+env.monitor.close()
+gym.scoreboard.api_key = data["api_key"]
+gym.upload('/tmp/frozenlake-experiment-9')
